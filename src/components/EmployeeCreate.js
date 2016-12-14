cd5eb78315b34f73420ba2a7bd4ff6f565cbcf2c
fbazzana <facundo.bazzana@widergy.com>
2016-12-14 12:07:46 -0300

    Add some features

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label="Name"
+            placeholder="Employee name"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label="Phone"
+            placeholder="555-555-555"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Text> Picker </Text>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+
+      </Card>
     );
   }
 }
